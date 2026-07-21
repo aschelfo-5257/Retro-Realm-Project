943f9594b2f3eb97f16390f37a2673fe9b145668
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-21 15:30:13 -0500

    __init__.py
    Initialize the database operations package with imports.

---
+++ database/__init__.py
@@ -0,0 +1,6 @@
+# Program Name: __init__.py
+# Author: Aaron Schelfo
+# Date Last Updated: July 21, 2026
+# Purpose: Initializes the database operations package.
+
+from .database_ops import add_platform, add_game, update_store, update_game_condition, delete_platform, execute_checkout_transaction
