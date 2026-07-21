56457742470f7195e10aa59b2f5335cd3c34053b
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-21 16:10:11 -0500

    __init__.py
    Initial commit of __init__.py to set up module.

---
+++ modules/__init__.py
@@ -0,0 +1,6 @@
+# Program Name: __init__.py
+# Author: Aaron Schelfo
+# Date Last Updated: July 21, 2026
+# Purpose: This is designed to eliminate the manual bottlenecks of Retro Realm Games
+
+from .reports import generate_game_report
