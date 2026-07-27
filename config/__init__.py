0c1cfdccd20a8a52e846a297b301322cd3795349
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-27 14:32:06 -0500

    __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,7 @@
-# Program Name: __init__.py
-# Author: Aaron Schelfo
-# Date last updated: July 21, 2026
-# Purpose: Exposes configuration properties across packages cleanly.
-
+"""
+Program Name: __init__.py
+Author: Aaron Schelfo
+Date last updated: July 21, 2026
+Purpose: Exposes configuration properties across packages cleanly.
+"""
 from .database_config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
