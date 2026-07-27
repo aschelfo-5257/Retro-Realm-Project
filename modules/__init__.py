0e1233fb17fda525c9fa9467c48024f469d0ac2c
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-27 14:29:00 -0500

    __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,8 @@
-# Program Name: __init__.py
-# Author: Aaron Schelfo
-# Date Last Updated: July 21, 2026
-# Purpose: This is designed to eliminate the manual bottlenecks of Retro Realm Games
+"""
+Program Name: __init__.py
+Author: Aaron Schelfo
+Date Last Updated: July 21, 2026
+Purpose: This is designed to eliminate the manual bottlenecks of Retro Realm Games
+"""
 
 from .reports import generate_game_report
