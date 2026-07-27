dfaad9f8b2c371827ac788227f006cef85b0c964
Aaron Schelfo <schelfoaaron918@gmail.com>
2026-07-27 14:30:12 -0500

    connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,9 @@
-# Program name: database_ops.py
-# Author: Aaron Schelfo
-# Date last updated: July 13, 2026
-# Purpose: Modular functions executing specialized inserts, modifications, and atomic transactions.
+"""
+Program name: database_ops.py
+Author: Aaron Schelfo
+Date last updated: July 13, 2026
+Purpose: Modular functions executing specialized inserts, modifications, and atomic transactions.
+"""
 
 from datetime import get_connection
 from mysql.connector import Error
